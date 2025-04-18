d8c5f7fe82945113a0b691bb05ae6f15cfe31269
Linnéa Toth <linnea@liroit.com>
2025-04-18 09:25:31 +0200

    add todos, add posts with comments, connect avatars

--- script/main.js
+++ script/main.js
@@ -1,4 +1,71 @@
-async function userData(){
+
+
+
+function clearInfoBox() { //clearing the container with extra info 
+    const cardInformation = document.querySelector(".card-information");
+    cardInformation.innerHTML = "";
+}
+
+function addTodos(todoArray) { //Adds users todo to the container with extra info 
+    const todoContainer = document.createElement("section");
+    const cardInformation = document.querySelector(".card-information");
+    const todoHeading = document.createElement("h2");
+    todoHeading.innerText = "To-do's"; //HÄR SKULLE VI KUNNA UTVECKLA FUNKTIONEN SÅ DEN SKRIVER TEX MRS DENNIS' TODOS
+    todoContainer.append(todoHeading);
+
+    todoArray.forEach((todo) => { //Looping through all of the todo-s, creating DOM-elements for them
+        const todoText = document.createElement("p");
+        const todoCompleted = document.createElement("i");
+        todoText.innerText = todo.title;
+        todoCompleted.classList.add("fa-solid");
+        todoCompleted.classList.add(todo.completed === true ? "fa-check" : "fa-xmark"); //✔️ if completed, ✖️ if not
+
+        todoContainer.append(todoText, todoCompleted);
+    })
+    cardInformation.append(todoContainer);
+}
+ 
+async function addPosts(postArray, amtComments) {
+    const postContainer = document.createElement("section");
+    const cardInformation = document.querySelector(".card-information");
+    const postHeading = document.createElement("h2");
+    postHeading.innerText = "Posts"; //HÄR SKULLE VI KUNNA UTVECKLA FUNKTIONEN SÅ DEN SKRIVER TEX MRS DENNIS' POSTS
+    postContainer.append(postHeading);
+
+    postArray.forEach(async (post) => {
+        const postCard = document.createElement("article");
+        const postHeading = document.createElement("h3");
+        postHeading.innerText = post.title;
+        const postContent = document.createElement("p");
+        postContent.innerText = post.body;
+        postCard.append(postHeading, postContent);
+
+
+        const commentCard = document.createElement("article");
+        let comments = await getPostComments(post.id);
+        comments = comments.slice(0, amtComments); //specifies the amount of comments shown
+        console.log(comments);
+
+        comments.forEach((comment) => {
+            const commentBody = document.createElement("p");
+            commentBody.innerText = comment.body;
+            commentBody.classList.add("comment");
+            commentCard.append(commentBody);
+        })
+        postCard.append(commentCard);        
+        postContainer.append(postCard);
+    })
+
+    cardInformation.append(postContainer);
+}
+
+async function getPostComments(postID) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postID}/comments`);
+    const comments = await response.json();
+    return comments;
+}
+
+async function userData() {
     const userContainer = document.querySelector('.card-container');
 
     try{
@@ -11,14 +78,16 @@ async function userData(){
 
         console.log(userData);
 
-        for (const user of userData){  /* Lägger till bild, grundinformationen, knapp och en tom div i html-strängen för varje användare. (Bilden är just nu en placeholder, vi får lösa den biten sen) */
+        for (const user of userData){  /* Lägger till bild, grundinformationen, knapp och en tom div i html-strängen för varje användare.  */
             userContainer.innerHTML +=`
             <div class="card">
-                <img src="#" alt="User Image">
+                <img src="./images/avatars/${user.id}.jpeg" alt="User Image" class="user-img">
                 <h2>${user.name}</h2>
-                <p>Username: ${user.username}</p>
-                <p>E-mail: ${user.email}</p>
-                <button class="show-btn" data-userid="${user.id}">Show more info</button>
+                <section>
+                    <p>Username: ${user.username}</p>
+                    <p>E-mail: ${user.email}</p>
+                </section>
+                <button class="show-btn" data-userid="${user.id}">Posts and to-do's</button>
                 <div class="more-info"></div>
             </div>                 
             `;     
@@ -50,17 +119,16 @@ async function userData(){
                     const posts = await postsRes.json();
                     const todos = await todosRes.json();
 
-                    console.log(posts);
-                    console.log(todos);
+                    console.log("posts: ", posts);
+                    console.log("todos: ", todos);
+
+                    clearInfoBox(); //clearing the info box from previous information
+                    addTodos(posts); //calls function that adds all of this users posts to the assigned container
+                    addPosts(posts, 3); //calls function that add posts, amount of comments shown as argument
+
                     
-                    /* - Här nedan tänker tänker jag att posts ska loopas igenom, hämta comments med postId och använda t.ex slice(0,3) för att endast ha med 3 st kommentarer.
-
-
-                    - Därefter lägga till posts, body från comments och todos i html-sträng och sedan in i den tomma diven ("more-info")
-
-
+                    /* 
                     - Ska catch skrivas ut som en alert eller som ett meddelande direkt i DOM med innerhtml?
-
                     */
 
                 }
